28d1270f5c0d2dbfe6ef09bdb9a31d94c4329729
mHamzaArain <hamzaarain1999@gmail.com>
2023-07-27 09:49:21 +0500

    formatted

--- 01_show_candle_once.py
+++ 01_show_candle_once.py
@@ -31,4 +31,4 @@ if __name__ == '__main__':
 
     ## Chart
     chart.show(block=True)
-    sleep(80)
+
--- 03_tick_backtest_live.py
+++ 03_tick_backtest_live.py
@@ -4,8 +4,6 @@ from lightweight_charts import Chart
 from datetime import datetime, timedelta
 import numpy as np
 import pandas_ta as ta
-
-
 import sqlite3
 
 def calculate_sma(data: pd.DataFrame, period: int = 50):
@@ -23,108 +21,62 @@ def check_time_difference(current_time, last_time, interval):
         return True
     else:
         return False
-    
-def apply_indicator(dataframe):
-    ## Candle Type
-    dataframe['green_candle'] = dataframe['close'] > dataframe['open']
-    dataframe['red_candle'] = dataframe['green_candle'] != 1
 
-    ## EMA 10
-    dataframe['ema10'] = ta.ema(dataframe.close, timeperiod=10)
-    
-    ## Shifting
-    dataframe['open_n-1'] = dataframe['open'].shift()
-    dataframe['high_n-1'] = dataframe['high'].shift()
-    dataframe['low_n-1'] = dataframe['low'].shift()
-    dataframe['close_n-1'] = dataframe['close'].shift()
-    dataframe['ema10_n-1'] = dataframe['ema10'].shift()
-    dataframe['green_candle_n-1'] = dataframe['green_candle'].shift()
-    dataframe['red_candle_n-1'] = dataframe['red_candle'].shift()
+if __name__ == '__main__':
+    ## Essentials
+    symbol = "LINAUSDT"
+    timeframe = "5m"
 
-    dataframe['open_n-2'] = dataframe['open'].shift(2)
-    dataframe['high_n-2'] = dataframe['high'].shift(2)
-    dataframe['low_n-2'] = dataframe['low'].shift(2)
-    dataframe['close_n-2'] = dataframe['close'].shift(2)
-    dataframe['ema10_n-2'] = dataframe['ema10'].shift(2)
-    dataframe['green_candle_n-2'] = dataframe['green_candle'].shift(2)
-    dataframe['red_candle_n-2'] = dataframe['red_candle'].shift(2)
+    conn_ohlc = sqlite3.connect(f'data/{symbol}_{timeframe}_ohlc.db')
+    conn_tick = sqlite3.connect(f'data/{symbol}_{timeframe}_tick.db')
 
-    dataframe['open_n-3'] = dataframe['open'].shift(3)
-    dataframe['high_n-3'] = dataframe['high'].shift(3)
-    dataframe['low_n-3'] = dataframe['low'].shift(3)
-    dataframe['close_n-3'] = dataframe['close'].shift(3)
-    dataframe['ema10_n-3'] = dataframe['ema10'].shift(3)
-    dataframe['green_candle_n-3'] = dataframe['green_candle'].shift(3)
-    dataframe['red_candle_n-3'] = dataframe['red_candle'].shift(3)
-    return dataframe
+    df_ohlc = pd.read_sql_query('SELECT * FROM ohlc_data', conn_ohlc)
+    df_ohlc['time']= pd.to_datetime(df_ohlc['time'])
+    df_ohlc.set_index('time', inplace=True)
+    df_ohlc.drop_duplicates(inplace=True)
 
-symbol = "LINAUSDT"
-timeframe = "5m"
+    df_tick = pd.read_sql_query('SELECT * FROM tick_data', conn_tick)
+    df_tick['time']= pd.to_datetime(df_tick['time'])
+    df_tick.set_index('time', inplace=True)
 
-conn_ohlc = sqlite3.connect(f'data/{symbol}_{timeframe}_ohlc.db')
-conn_tick = sqlite3.connect(f'data/{symbol}_{timeframe}_tick.db')
+    start_time = "2023-07-22 03:59:03"
+    end_time = "2023-07-23 12:29:41"
+    beyond_time = start_time
 
-df_ohlc = pd.read_sql_query('SELECT * FROM ohlc_data', conn_ohlc)
-df_ohlc['time']= pd.to_datetime(df_ohlc['time'])
-df_ohlc.set_index('time', inplace=True)
-df_ohlc.drop_duplicates(inplace=True)
+    df1 = df_ohlc[:start_time]
+    df2 = df_ohlc[:end_time]
+    df3 = df_tick[start_time:end_time]
 
-df_tick = pd.read_sql_query('SELECT * FROM tick_data', conn_tick)
-df_tick['time']= pd.to_datetime(df_tick['time'])
-df_tick.set_index('time', inplace=True)
+    df1.reset_index(inplace=True)
+    df3.reset_index(inplace=True)
+    df3['volume'] = [0 for i in range(df3.shape[0])]
+
+    ## Chart
+    chart = Chart()
+    chart.set(df1)
+    chart.precision(6)
+
+    ## Line
+    period = 10
+    line = chart.create_line()
+    sma_data = calculate_sma(df1, period)
+    line.set(sma_data, name='SMA 10')
+
+    ## Chart
+    # chart.show(block=True)
+    chart.show()
+
+    last_time = df1['time'].iloc[-1]
 
 
-start_time = "2023-07-22 03:59:03"
-end_time = "2023-07-23 12:29:41"
-beyond_time = start_time
+    for i, series in df3.iterrows():
+        if check_time_difference(current_time=pd.to_datetime(series['time']), last_time=pd.to_datetime(last_time), interval=5):
+            last_time = series['time']
+            df = df2[:last_time].copy()
+            df.reset_index(inplace=True)
 
-df1 = df_ohlc[:start_time]
-df2 = df_ohlc[start_time:end_time]
-df3 = df_tick[start_time:end_time]
-
-df1.reset_index(inplace=True)
-df3.reset_index(inplace=True)
-df3['volume'] = [0 for i in range(df3.shape[0])]
-
-
-chart = Chart()
-
-chart.set(df1)
-
-line = chart.create_line()
-sma_data = calculate_sma(df1, period=10)
-line.set(sma_data, name='SMA 10')
-
-chart.show()
-
-# # df_ohlc['time']= pd.to_datetime(df_ohlc['time'])
-
-
-# last_close = df1.iloc[-1]
-# df1.set_index('time', inplace=True)
-
-last_time = df1['time'].iloc[-1]
-
-# print(type(last_time))
-
-for i, series in df3.iterrows():
-#     chart.update(series)
-
-    if check_time_difference(current_time=pd.to_datetime(series['time']), last_time=pd.to_datetime(last_time), interval=5):
-        last_time = series['time']
-        # print(last_time)
-
-        df = df_ohlc[:last_time].copy()
-        df.reset_index(inplace=True)
-        sma_data = calculate_sma(df, period=10)
-
-
-    # if series['price'] >  and last_close < 20:
-    #     chart.marker(text='The price crossed $20!')
-    
-#     # last_close = series['close']
-    chart.update_from_tick(series)   
-    # sleep(1)
-         
-    # sleep(0.000001)
-
+            recalculated_sma = calculate_sma(df, period)
+            line_value = recalculated_sma.iloc[-1]
+            line.update(line_value.rename({'SMA 10': 'value'}))
+            sleep(0.1)
+        chart.update_from_tick(series)
