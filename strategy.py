b08809f868d4a99f1e5c5182027fa4ec3086e9bb
mHamzaArain <hamzaarain1999@gmail.com>
2023-07-29 12:47:09 +0500

    unnecessary things

--- strategy.py
+++ strategy.py
@@ -1,11 +1,20 @@
+import pandas_ta as ta
+
+
 class MyAwesomeStrategy:
-    def populates_essentials(self):
+    def populate_essentials(self, dataframe):
+        dataframe['sma10'] = ta.sma(dataframe.close, timeperiod=10)
+        dataframe['green_candle'] = dataframe['close'] > dataframe['open']
+        dataframe['red_candle'] = dataframe['green_candle'] != 1
+        return dataframe
+
+    def buy_ohlc_condition(self, dataframe):
+        dataframe['buy_screener_condition_1'] = ( (dataframe['red_candle']==True) & (dataframe['low'] > dataframe['ema10']) )
+
+        dataframe['buy_ohlc_condition'] = (dataframe['green_candle'])
         return True
 
-    def buy_ohlc_condition(self):
-        return True
-
-    def sell_ohlc_condition(self):
+    def sell_ohlc_condition(self, dataframe):
         return True
 
     def buy_tick_condition(self):
